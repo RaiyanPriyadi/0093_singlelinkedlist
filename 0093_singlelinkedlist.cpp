592804ee5f671b637159d893b7b140e9c1262969
RaiyanPriyadi <dwi.raiyan.ft25@mail.umy.ac.id>
2026-04-23 18:23:06 +0700

    class public

--- 0093_singlelinkedlist.cpp
+++ 0093_singlelinkedlist.cpp
@@ -7,5 +7,27 @@ class node
 public:
     int noMhs;
     node *next;
+}
+
+class LinkedList
+{
+    Node *START;
+
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        if (START == NULL )
+    }
+};
 
 
